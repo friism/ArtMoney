e26e8bd3eca55f317670e6342a1a891ee14f3011
Michael Friis <friism@gmail.com>
2013-01-24 21:17:27 -0800

    support fetching all areas of spending

--- ArtMoney/Program.cs
+++ ArtMoney/Program.cs
@@ -3,6 +3,7 @@ using ArtMoney.Model;
 using ArtMoney.Persistence;
 using HtmlAgilityPack;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -11,32 +12,51 @@ namespace ArtMoney
 	public class Program
 	{
 		private const string _urlFormat =
-			"kunststoette/tildelinger/?tx_lftilskudsbase_pi5%5Barea%5D=2&tx_lftilskudsbase_pi5%5Bstype%5D=0&tx_lftilskudsbase_pi5%5Byear%5D={0}&tx_lftilskudsbase_pi5%5Bsword%5D=&tx_lftilskudsbase_pi5%5Border%5D=dato%20desc&tx_lftilskudsbase_pi5%5Bpage%5D={1}";
+			"kunststoette/tildelinger/?tx_lftilskudsbase_pi5%5Barea%5D={0}&tx_lftilskudsbase_pi5%5Bstype%5D=0&tx_lftilskudsbase_pi5%5Byear%5D={1}&tx_lftilskudsbase_pi5%5Bsword%5D=&tx_lftilskudsbase_pi5%5Border%5D=dato%20desc&tx_lftilskudsbase_pi5%5Bpage%5D={2}";
+
+		private static readonly Dictionary<int, string> _areaMap = new Dictionary<int, string>
+		{
+			{ 1, "Arkitektur" },
+			{ 2, "Billedkunst" },
+			{ 4, "Film" },
+			{ 9, "Internationalt" },
+			{ 5, "Kunsthåndværk og design" },
+			{ 6, "Litteratur" },
+			{ 7, "Musik" },
+			{ 8, "Scenekunst" },
+		};
 
 		static void Main(string[] args)
 		{
-			Enumerable.Range(2010, 3).AsParallel().ForAll(x =>
-				GetYear(x)
+			Enumerable.Range(2010, 3).AsParallel().WithDegreeOfParallelism(1).ForAll(year =>
+				{
+					foreach (var area in _areaMap.Keys)
+					{
+						GetYear(year, area);
+					}
+				}
 			);
 		}
 
-		private static void GetYear(int year)
+		private static void GetYear(int year, int area)
 		{
-			var firstPage = GetContent(string.Format(_urlFormat, year, 1));
-			var pageCount = int.Parse(
-				firstPage.DocumentNode.SelectNodes("//*[@id='c45806']/div/div/a")
-					.Last().InnerText
-				);
+			var firstPage = GetContent(string.Format(_urlFormat, area, year, 1));
+			var lastPageString = firstPage.DocumentNode.SelectNodes("//*[@id='c45806']/div/div/a")
+					.Last().InnerText;
+			if (!string.IsNullOrEmpty(lastPageString))
+			{
+				var pageCount = int.Parse(lastPageString);
 
-			Enumerable.Range(1, pageCount - 1)
-				.AsParallel().ForAll(x => FetchPage(year, x));
+				Enumerable.Range(1, pageCount - 1)
+					.AsParallel().ForAll(pageNumber => FetchPage(year, area, pageNumber));
+			}
 		}
 
-		private static void FetchPage(int year, int pageNumber)
+		private static void FetchPage(int year, int area, int pageNumber)
 		{
 			using (var context = new Context())
 			{
-				var page = GetContent(string.Format(_urlFormat, year, pageNumber));
+				var page = GetContent(string.Format(_urlFormat, area, year, pageNumber));
 				var rows = page.DocumentNode.SelectNodes("//div[@class='list']/table/tbody/tr");
 				foreach (var row in rows)
 				{
@@ -51,7 +71,7 @@ namespace ArtMoney
 					context.Grants.Add(new Grant
 					{
 						Amount = amount,
-						Area = "Billedkunst",
+						Area = _areaMap[area],
 						ArtistName = artistName,
 						Date = date,
 						Giver = giver,
@@ -61,7 +81,7 @@ namespace ArtMoney
 					});
 				}
 				context.SaveChanges();
-				Console.WriteLine("Retrieved {0} page {1}", year, pageNumber);
+				Console.WriteLine("Retrieved {2}, {0} page {1}", year, pageNumber, _areaMap[area]);
 			}
 		}
 
